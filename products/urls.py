c90813328754c37b1603cf8b5392e7a99e2200a3
Steven Sisavath <stevenlsisavath@gmail.com>
2022-11-11 13:38:10 -0600

    Created Method to GET All Products in Database + Updated URLs to inlcude pk

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list)
+    path('', views.product_list),
+    path('<int:pk>/', views.product_list)
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def product_list(request):
-    return Response('ok')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many = True)
+
+
+    return Response(serializer.data)
