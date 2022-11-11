f8dd2e3a638b8ccf038ae28206225757179a63a3
Steven Sisavath <stevenlsisavath@gmail.com>
2022-11-11 14:17:26 -0600

    Added PUT Functionality in Views File to Update Products Database

--- products/views.py
+++ products/views.py
@@ -19,9 +19,14 @@ def product_list(request):
         serializer.save()
         return Response(serializer.errors, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk) #(model, pk=pk)
-    request.method == 'GET'
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
